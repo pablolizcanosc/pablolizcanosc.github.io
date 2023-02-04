4e2ec249e0c67ee817550e1d19d9619e7a2844ef
Pablo Lizcano SC <pablod.lizcanosc@gmail.com>
2023-02-04 20:04:56 +0100

    updated javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-    $("#header").load("header.html");
-  });
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "header.html", true);
+xhr.onreadystatechange = function() {
+  if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+    document.body.insertAdjacentHTML("afterbegin", this.responseText);
+  }
+};
+xhr.send();
